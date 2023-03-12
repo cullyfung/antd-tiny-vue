0c9a15f9d38bd068475f9e3e2ce57db2b1f37ccf
cullyfung <cullyfung@gmail.com>
2023-03-12 19:34:44 +0800

    chore: demo

---
+++ docs/demos/button/basic.vue
@@ -0,0 +1,16 @@
+<!-- <docs>
+  ---
+  title: 基础按钮
+  ---
+  这是一个Button的基础按钮
+</docs> -->
+
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <a-button>这是按钮</a-button>
+  </div>
+</template>
+
+<style scoped></style>
